e4157dba7e59942681f42eb7bc282f4e06abd206
Adonis95 <Sober4202020@outlook.com>
2025-03-23 19:26:32 -0600

    testing connection

--- thing.py
+++ thing.py
@@ -6,9 +6,9 @@ import pandas as pd
 load_dotenv()
 # C:\Users\Sober\Downloads
 
-csv_file_path = r"C:\\Users\\Sober\\Downloads\\freelancer_earnings_bd.csv"
+#csv_file_path = r"C:\\Users\\Sober\\Downloads\\freelancer_earnings_bd.csv"
 
-df = pd.read_csv(csv_file_path)
+#df = pd.read_csv(csv_file_path)
 
 
 
@@ -26,16 +26,18 @@ engine = create_engine(f"mssql+pyodbc://{username}:{password}@{server}/{database
 try:
     with engine.connect() as conn:
         print("✅ Connection successful!")
-        query = "SELECT * FROM Homes"
-        df = pd.read_sql(query, engine)
+       
+       
+       # query = "SELECT * FROM Homes"
+       # df = pd.read_sql(query, engine)
 
-        print
+        
        # df.to_sql("Homes", con=engine, if_exists="replace", index=False)
         #print("Data has now been inserted")
 
-        df.to_excel("Another_output.xlsx", index=False)
-        print("The data has now been saved to an Excel file")
-        
+    # df.to_excel("Another_output.xlsx", index=False)
+    #    print("The data has now been saved to an Excel file")
+
 
 except Exception as e:
     print(f"❌ Connection failed: {e}")
